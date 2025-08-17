391f6e580f30c06d71fc12fc51bf96ac6708dbdf
Kenny7SP <goodmorningvo3@gmail.com>
2025-08-17 22:16:07 +0300

    Recreate logic

--- script.js
+++ script.js
@@ -1,117 +1,135 @@
 const buttons = document.querySelectorAll('.button');
-const numbers = document.querySelectorAll('.num');
 const interactions = document.querySelectorAll('.interaction');
-const operators = document.querySelectorAll('.operator');
-const answer = document.querySelector('#answer');
-let firstOperand = '';
-let secondOperand = '';
-let currentTask = '';
-let currentOperator = '';
-let currentAnswer = '';
-let arguments = [];
-let flag = false;
-let flag2 = false;
+const equal = document.querySelector('#equal');
+const output = document.querySelector('#output');
+let expression = '';
+let currentInput = '';
+let lastInput = '';
+let parts = [];
+let operators = [];
+const specMarks = ['+', '-', '*', '/', '=', '.'];
+const specNums = /[0-9]/;
 
 function addSymbol(symbol) {
-    const operators = ["+", "-", "*", "/"];
-    if (operators.includes(symbol) && operators.includes(expression.slice(-1))) {
-      return; 
-    }
-    currentTask += symbol;
-    answer.textContent = expression;
+  lastInput = currentInput;
+  parts = expression.split(/[\+\-\*\/]/); 
+  let lastNumber = parts[parts.length-1];
+  if (symbol === '=') return;
+  if (expression.length === 0 && ((specMarks.includes(symbol) && symbol !== '-'))) {
+    return;
+  };
+  if ((specMarks.includes(symbol)) && (specMarks.includes(lastInput))) {
+    return;
+  };
+  if (lastNumber === '0' && symbol === '0') return;  
+  if (lastNumber === '0' && (symbol !== '.')) expression = expression.replace(/.$/, '');
+  if (lastNumber.includes('.') && symbol === '.') return;
+  currentInput = symbol;
+  expression += symbol;
+  output.textContent = expression;
+  parts = expression.split(/[\+\-\*\/]/); 
+}
+
+function operate(parts, operators) {
+  console.log(operators);
+  operands = parts.reverse();
+  let result = null;
+  for (let i of operators) {
+    let firstOperand = Number(parts.pop());
+    let secondOperand = Number(parts.pop());
+    console.log(i);
+    switch(i) {
+      case '+':
+        result = firstOperand + secondOperand;
+        break;
+      case '-':
+        result = firstOperand - secondOperand;
+        break;
+      case '*' :
+        result = firstOperand * secondOperand;
+        break;
+      case '/' :
+        result = firstOperand / secondOperand;
+        break;
+    };
+    parts.push(result);
+  };
+  result = parts[0];
+  if (result % 1 !== 0) result = Math.round(result * 100_000) / 100_000;
+  console.log(result);
+  return String(result);
 }
 
 
 buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        let arg = button.textContent;
-        currentTask += arg;
-        answer.textContent = currentTask;
-    })
-})
-numbers.forEach(num => {
-    num.addEventListener('click', (e) => {
-        let arg = num.textContent;
-        if (!flag) firstOperand += arg;
-        else secondOperand += arg;
-        console.log(getvalues());
-    })
-})
-operators.forEach(operator => {
-    operator.addEventListener('click', (e) => {
-        let arg = operator.textContent;
-        if (secondOperand != '' && arg != '=') {
-            currentAnswer = getAnswer(firstOperand, secondOperand, arg);
-            firstOperand = currentAnswer;
-            currentOperator = arg;
-            secondOperand = '';
-            currentTask = `${currentAnswer} ${currentOperator} `;
-            answer.textContent = currentTask;
-            flag2 = true;
-        }
-        if ((!flag || currentOperator === '=') && !flag2) currentOperator = arg;
-        if (flag && secondOperand != '') {
-            console.log(getvalues());
-            console.log('done');
-            currentAnswer = getAnswer(firstOperand, secondOperand, currentOperator);
-            console.log(currentAnswer);
-            firstOperand = currentAnswer;
-            secondOperand = '';
-            currentOperator = '';
-            currentTask = `${currentAnswer} ${currentOperator} `;
-            answer.textContent = currentTask;
-            flag = false;
-        }
-        flag2 = false;
-        if (currentOperator !== '') flag = true;
-    })
+  button.addEventListener('click', (e) => {
+    let arg = button.textContent;
+    addSymbol(arg);
+  });
+});
+
+document.addEventListener('keydown', (e) => {
+  let arg = e.key;
+  console.log(e.key);
+  if (arg === 'Enter') {
+    parts = expression.split(/[\+\-\*\/]/); 
+    operators = expression.split(/[0-9\.]/);
+    parts = parts.filter(function (e) { 
+      return e;
+    });
+    operators = operators.filter(function (e) { 
+      return e;
+    });
+    
+    if ((parts.length - operators.length) === 1 && operators.length !== 0) {
+      expression = operate(parts, operators);
+      output.textContent = expression;
+    };
+  };
+  if (arg === 'Backspace') {
+    expression = expression.replace(/.$/, '');
+    output.textContent = expression;
+    if (!specMarks.includes(currentInput)) parts.pop();
+    currentInput = expression[expression.length-1];
+  };
+  console.log(arg)
+  if (specMarks.includes(arg) || specNums.test(arg)) addSymbol(arg);
 })
 
-function getAnswer(firstOperand, secondOperand, operator) {
-    let result = null;
-    firstOperand = parseInt(firstOperand);
-    secondOperand = parseInt(secondOperand);
-    switch(operator) {
-        case '+' :
-            result = firstOperand + secondOperand;
-            break;
-        case '-' :
-            result = firstOperand - secondOperand;
-            break;
-        case 'x' :
-            result = firstOperand * secondOperand;
-            break;
-        case '÷' :
-            result = firstOperand / secondOperand;
-            break;
-    }
-    console.log(firstOperand, secondOperand, operator);
-    if (result % 1 != 0) result = (Math.floor(result * 100000) / 100000);
-    return result;
-}
-function getvalues() {
-    console.log(firstOperand, secondOperand, ' values');
-    console.log(currentOperator);
-    console.log(flag);
+equal.addEventListener('click', (e) => {
+  parts = expression.split(/[\+\-\*\/]/); 
+  operators = expression.split(/[0-9\.]/);
 
-}
+  parts = parts.filter(function (e) { 
+    return e;
+  });
+  operators = operators.filter(function (e) { 
+    return e;
+  });
+  
+  if ((parts.length - operators.length) === 1 && operators.length !== 0) {
+    expression = operate(parts, operators);
+    output.textContent = expression;
+  };
+});
 
-// 0 / 0
-// number / 0
-// add 1+1+1
-// operator as a first enter
-// 0 as a first enter 
-// 0 as a first enter as number
-
-/*
-get num
-if ener == operator
-get second num
-if operator entered
-if operator != '='
-do math 
-first operand = result of math
-second operand = ''
-add current operator && add him to current answer
-
-*/
+interactions.forEach(interaction => {
+  interaction.addEventListener('click', (e) => {
+    target = interaction.textContent;
+    switch(target) {
+      case 'AC':
+        expression = '';
+        currentInput = '';
+        lastInput = '';
+        currentInput = '';
+        output.textContent = '';
+        break;
+      case 'C':
+        expression = expression.replace(/.$/, '');
+        output.textContent = expression;
+        if (!specMarks.includes(currentInput)) parts.pop();
+        currentInput = expression[expression.length-1];
+        break;
+    };
+  });
+});
